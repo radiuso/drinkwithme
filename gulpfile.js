3610d83cf85289acd226e98c0ecbc8811ad46b5a
tvandermeersch <tvandermeersch@alteca.fr>
2015-10-02 17:09:20 +0200

    Init codeIgniter project with rest api + init npm, bower, gulp

---
+++ gulpfile.js
@@ -0,0 +1,8 @@
+var gulp        = require('gulp'),
+    minimist    = require('minimist'),
+    summary     = require('jshint-summary'),
+    del         = require('del'),
+    plugins     = require('gulp-load-plugins')();
+
+var paths = { };
+
