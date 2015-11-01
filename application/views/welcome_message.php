d2a3f9b9e2198c8e474997cbc93b3d3645a0b84b
Thomas <thomas@alpagap.com>
2015-11-01 19:17:42 +0100

    create welcome page React way

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -1,99 +1,43 @@
-<?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-?><!DOCTYPE html>
-<html lang="en">
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+<!DOCTYPE html>
+<html>
 <head>
     <meta charset="utf-8">
     <title>Welcome to CodeIgniter</title>
-
-    <style type="text/css">
-
-    ::selection { background-color: #E13300; color: white; }
-    ::-moz-selection { background-color: #E13300; color: white; }
-
-    body {
-        background-color: #FFF;
-        margin: 40px;
-        font: 16px/20px normal Helvetica, Arial, sans-serif;
-        color: #4F5155;
-        word-wrap: break-word;
-    }
-
-    a {
-        color: #003399;
-        background-color: transparent;
-        font-weight: normal;
-    }
-
-    h1 {
-        color: #444;
-        background-color: transparent;
-        border-bottom: 1px solid #D0D0D0;
-        font-size: 24px;
-        font-weight: normal;
-        margin: 0 0 14px 0;
-        padding: 14px 15px 10px 15px;
-    }
-
-    code {
-        font-family: Consolas, Monaco, Courier New, Courier, monospace;
-        font-size: 16px;
-        background-color: #f9f9f9;
-        border: 1px solid #D0D0D0;
-        color: #002166;
-        display: block;
-        margin: 14px 0 14px 0;
-        padding: 12px 10px 12px 10px;
-    }
-
-    #body {
-        margin: 0 15px 0 15px;
-    }
-
-    p.footer {
-        text-align: right;
-        font-size: 16px;
-        border-top: 1px solid #D0D0D0;
-        line-height: 32px;
-        padding: 0 10px 0 10px;
-        margin: 20px 0 0 0;
-    }
-
-    #container {
-        margin: 10px;
-        border: 1px solid #D0D0D0;
-        box-shadow: 0 0 8px #D0D0D0;
-    }
-    </style>
+    <script src="application/assets/libs/react/react.js"></script>
+    <script src="application/assets/libs/react/react-dom.js"></script>
+    <script src="application/assets/libs/babel/browser.js"></script>
+    <script src="application/assets/libs/jquery/dist/jquery.js"></script>
 </head>
 <body>
+    <div id="content"></div>
+    <script type="text/babel">
+        var Welcome = React.createClass({
+            render: function() {
+                return (
+                    <div className="commentBox">
+                        Hello, world! I am a CommentBox.
+                        <div id="body">
+                            <h2><a href="">REST Server Tests</a></h2>
+                            <h2><a href={this.props.doc}>REST Server Documentation</a></h2>
+                            <p>The page you are looking at is being generated dynamically by CodeIgniter.</p>
 
-<div id="container">
-    <h1>Welcome to CodeIgniter!</h1>
-
-    <div id="body">
-
-        <h2><a href="<?php echo site_url('rest-server'); ?>">REST Server Tests</a></h2>
-
-        <?php if (file_exists(FCPATH.'documentation/index.html')) : ?>
-        <h2><a href="<?php echo base_url('documentation/index.html'); ?>" target="_blank">REST Server Documentation</a></h2>
-        <?php endif ?>
-
-        <p>The page you are looking at is being generated dynamically by CodeIgniter.</p>
-
-        <p>If you would like to edit this page you'll find it located at:</p>
-        <code>application/views/welcome_message.php</code>
-
-        <p>The corresponding controller for this page is found at:</p>
-        <code>application/controllers/Welcome.php</code>
-
-        <?php if (file_exists(FCPATH.'user_guide/index.html')) : ?>
-        <p>If you are exploring CodeIgniter for the very first time, you should start by reading the <a href="<?php echo base_url('user_guide/index.html'); ?>" target="_blank">User Guide</a>.</p>
-        <?php endif ?>
-    </div>
-
-    <p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
-</div>
-
+                            <p>If you would like to edit this page youll find it located at:</p>
+                            <code>application/views/welcome_message.php</code>
+                            <p>The corresponding controller for this page is found at:</p>
+                            <code>application/controllers/Welcome.php</code>
+                            <p>If you are exploring CodeIgniter for the very first time, you should start by reading the <a href="" target="_blank">User Guide</a>.</p>
+                        </div>
+                        <p className="footer">
+                            Page rendered in <strong>{elapsed_time}</strong> seconds. </p>
+                    </div>
+                );
+            }
+        });
+        ReactDOM.render(
+          <Welcome doc="<?php echo base_url('documentation/index.html'); ?>" />,
+          document.getElementById('content')
+        );
+    </script>
 </body>
 </html>
